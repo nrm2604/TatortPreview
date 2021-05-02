8798a149745802d3f7be5568b9a9bea630d00183
nrm2604 <51862097+nrm2604@users.noreply.github.com>
2021-05-02 22:27:09 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,27 @@
+let helloWorldPopup;
+
+// Open the popup when we enter a given zone
+helloWorldPopup = WA.onEnterZone('StreamingZone', () => {
+    WA.openPopup("Popup", 'Hello world!', [{
+        label: "Close",
+        className: "primary",
+        callback: (popup) => {
+            // Close the popup when the "Close" button is pressed.
+            popup.close();
+        }
+    }]);
+});
+
+// Close the popup when we leave the zone.
+
+WA.onLeaveZone('StreamingZone', () => {
+    helloWorldPopup.close();
+});
+
+
+let Exit;
+
+// Exit to webpage when we enter a given zone
+Exit = WA.onEnterZone('ExitZone', () => {
+    WA.goToPage('http://www.strassenmeyer.de/NDR-DEMO_02/index.htm?media=Foyer_NDR');  
+});
