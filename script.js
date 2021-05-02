845844e816c50073c06312cc01a21c48751aaad9
nrm2604 <51862097+nrm2604@users.noreply.github.com>
2021-05-02 12:02:58 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,25 @@
-WA.sendChatMessage('Hello world', 'Mr Robot');
-alert('Hallo Welt!');
-console.log("In die Browser-Console geschrieben: ")
+class Popup {
+    /**
+     * Closes the popup
+     */
+    close() { };
+}
+
+let helloWorldPopup;
+
+// Open the popup when we enter a given zone
+helloWorldPopup = WA.onEnterZone('popup', () => {
+    WA.openPopup("popupRectangle", 'Hello world!', [{
+        label: "Close",
+        className: "primary",
+        callback: (popup) => {
+            // Close the popup when the "Close" button is pressed.
+            popup.close();
+        }
+    });
+}]);
+
+// Close the popup when we leave the zone.
+WA.onLeaveZone('popup', () => {
+    helloWorldPopup.close();
+});
