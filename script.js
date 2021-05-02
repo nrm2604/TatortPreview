a0bfaf2b36c0317cca06a48bb0976db0358e5a41
nrm2604 <51862097+nrm2604@users.noreply.github.com>
2021-05-02 22:40:50 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 let helloWorldPopup;
 
 // Open the popup when we enter a given zone
+
 helloWorldPopup = WA.onEnterZone('StreamingZone', () => {
-    WA.openPopup("Popup", 'Hello world!', [{
+    WA.openPopup("popupRectangle", 'Hello world!', [{
         label: "Close",
         className: "primary",
         callback: (popup) => {
