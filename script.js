a38d15187a0176ad6fe80c34c6e306ae23a3e344
nrm2604 <51862097+nrm2604@users.noreply.github.com>
2021-05-03 16:33:27 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,8 +2,8 @@ let helloWorldPopup;
 
 // Open the popup when we enter a given zone
 helloWorldPopup = WA.onEnterZone('StreamingZone', () => {
-    WA.openPopup("popupRectangle", WA.goToPage"https://vimeo.com/event/949260/embed/90aa3a1dd0", [{
-        label: "Schliessen",
+    WA.openPopup("popupRectangle", goToPage("https://vimeo.com/event/949260/embed/90aa3a1dd0"), [{
+        label: "Schlie&#223en",
         className: "primary",
         callback: (popup) => {
             // Close the popup when the "Close" button is pressed.
