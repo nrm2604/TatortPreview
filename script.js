9f901c2e06b50fe3bbed3ef1578dcd5416f20cbd
nrm2604 <51862097+nrm2604@users.noreply.github.com>
2021-05-03 15:22:18 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,12 @@
 let helloWorldPopup;
 
-// Open the popup when we enter a given zone
+// Open the Website when we enter a given zone
 
 helloWorldPopup = WA.onEnterZone('StreamingZone', () => {
-    WA.openPopup("popupRectangle", 'Hello world!', [{
-        label: "Close",
-        className: "primary",
-        callback: (popup) => {
-            // Close the popup when the "Close" button is pressed.
-            popup.close();
-        }
-    }]);
-});
+    WA.openCoWebsite("http://localhost/streampopup.html", [{
+        
 
-// Close the popup when we leave the zone.
+// Close Website when we leave the zone.
 
 WA.onLeaveZone('StreamingZone', () => {
     helloWorldPopup.close();
